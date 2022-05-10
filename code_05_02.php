533a3e22196b8aac41277e2f0adac9da0aad4293
Arkadiusz Cieśla <77988555+CodeSeven-7@users.noreply.github.com>
2022-05-10 17:26:07 +0200

    Add files via upload
    Lesson 5

---
+++ code_05_02.php
@@ -0,0 +1,7 @@
+<?php
+print "Moja wersja PHP: ". PHP_VERSION .PHP_EOL;
+print "Moj system operacyjny: ". PHP_OS. "(". PHP_OS_FAMILY. ")" .PHP_EOL;
+print "Minimalna wartosc typu INT: ". PHP_INT_MIN .PHP_EOL;
+print "Maksymalna wartosc typu INT: ". PHP_INT_MAX .PHP_EOL;
+print "Minimalna wartosc typu FLOAT: ". PHP_FLOAT_MIN .PHP_EOL;
+print "Maksymalna wartosc typu FLOAT: ". PHP_FLOAT_MAX .PHP_EOL;
---
+++ code_05_03.php
@@ -0,0 +1,4 @@
+<?php
+print "Nazwa pliku z cala sciezka: ". __FILE__ .PHP_EOL;
+print "Nazwa katalogu, w ktorym plik sie znajduje: ". __DIR__ .PHP_EOL;
+print "Numer tej linii w kodzie: ". __LINE__;
